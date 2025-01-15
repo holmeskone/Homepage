9e8e9f0f099a2385093c0cc5cc079e042b055162
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-15 09:40:30 +0100

    Removed unnecessary actions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import "./style.css"; // Import CSS styles 
-
-console.log('hello world')
-
-const content = document.getElementById('content');
-const tester = document.createElement('p');
-tester.textContent = 'Hello, this is a test';
-content.appendChild(tester);
+import "./style.css"; // Import CSS styles
